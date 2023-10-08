6a100dead01dd1909d5eac1b48e36b7116867074
Den <8770532@gmail.com>
2023-10-08 16:35:36 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LogoSvg from "./images/image_logo1.svg"
+import BasketSvg from "./images/basket.svg"
+import ImagePng from "./images/image 2.png"
+import FertilizerPng from "./images/16-320x370 1.png"
+import SepticPng from "./images/16-320x370 2.png"
+import PlantingPng from "./images/16-320x370 3.png"
+import InventoryPng from "./images/16-320x370 4.png"
+import HobitPng from "./images/hobit.png"
+import DecorativeBridgePng from "./images/bridge_1.png"
+import FlowerBasketPng from "./images/flower_1.png"
+import AquariumPng from "./images/castle_1.png"
+import InstagramPng from "./images/instagram 3.png"
+import WhatappPng from "./images/whatsapp 1.png"
 import './index.css';
 
 
@@ -13,109 +25,109 @@ root.render(
             <div class="icon">
                 <img src={LogoSvg} alt=""/>
             </div>
-            <button class="green-btn">Каталог</button>
+            <button class="green-btn">Catalog</button>
         </div>
         <div class="right">
-            <div class="active">Категории</div>
-            <div>Купон</div>
-            <div>Акции</div>
-            <div>Контакты</div>
+            <div class="active">Categories</div>
+            <div>Coupon</div>
+            <div>Sale</div>
+            <div>Contact</div>
             <div>
-                <img src="images/basket.svg" alt=""/>
+                <img src={BasketSvg} alt=""/>
             </div>
         </div>
     </header>
     <main>
         <section class="group-hero">
             <div class="left">
-                <div class="title">Распродажа <br/>
-                    <span>в честь нового сезона</span>
+                <div class="title">Sale <br/>
+                    <span>New season</span>
                 </div>
                 <div class="btns">
-                    <button>Все акции</button>
-                    <button class="secondary">Подробнее</button>
+                    <button>Sale</button>
+                    <button class="secondary">Learn more</button>
                 </div>
 
             </div>
             <div class="right">
-                <img src="images/image 2.png" alt=""/>
+                <img src={ImagePng} alt=""/>
             </div>
         </section>
         <section class="categories">
             <div class="top">
-                <div class="title">Категории</div>
-                <button>Все категории</button>
+                <div class="title">Catalog</div>
+                <button>All Categories</button>
             </div>
             <div class="botton">
                 <div class="item">
-                    <img src="./images/16-320x370 1.png" alt=""/>
-                    <div class="item-name">Удобрение</div>
+                    <img src={FertilizerPng} alt=""/>
+                    <div class="item-name">Fertilizer</div>
                 </div>
                 <div class="item">
-                    <img src="./images/16-320x370 2.png" alt=""/>
-                    <div class="item-name">Средства Защиты и септики</div>
+                    <img src={SepticPng} alt=""/>
+                    <div class="item-name">Protective products and septic tanks</div>
                 </div>
                 <div class="item">
-                    <img src="./images/16-320x370 3.png" alt=""/>
-                    <div class="item-name">Посадочный материал</div>
+                    <img src={PlantingPng} alt=""/>
+                    <div class="item-name">Planting material</div>
                 </div>
                 <div class="item">
-                    <img src="./images/16-320x370 4.png" alt=""/>
-                    <div class="item-name"> Инструменты и Инвентарь</div>
+                    <img src={InventoryPng} alt=""/>
+                    <div class="item-name">Tools and Inventory</div>
                 </div>
             </div>
         </section>
         <section class="sales">
             <div class="left">
-                <img src="./images/hobit.png" alt=""/>
+                <img src={HobitPng} alt=""/>
             </div>
             <div class="right">
                 <div class="title">
-                    <div class="title-top"> Скидка <span>5%</span> </div>
-                    <div class="title-botton">на первый заказ</div>
+                    <div class="title-top"> 5%<span> off</span> </div>
+                    <div class="title-botton">  on the first order</div>
                 </div>
                 <input type="text" placeholder="+7"/>
-                <button> Получить скидку</button>
+                <button> Get a discount</button>
             </div>
         </section>
         <section class="stock">
-            <div class="stock-item">Акции</div>
+            <div class="stock-item">Sale</div>
             <div class="stock-container">
                 <div class="container-item">
                     <div class="thumbanail">
-                        <img src="./images/bridge_1.png" alt=""/>
-                        <button>Добавить в корзину</button>
+                        <img src={DecorativeBridgePng} alt=""/>
+                        <button>Add to cart</button>
                     </div>
                     <div class="price">
-                        <span> 145000<span class="p">p</span></span>
-                        <span class="og-price">15400<span class="p">p</span></span>
+                        <span> 500<span class="p">$</span></span>
+                        <span class="og-price">1000<span class="p">$</span></span>
                         <span class="sale">-7%</span>
                     </div>
-                    <div class="title">Мостик декоративный кованый</div>
+                    <div class="title">Decorative forged bridge</div>
                 </div>
                 <div class="container-item">
                     <div class="thumbanail">
-                        <img src="./images/flower_1.png" alt=""/>
-                        <button>Добавить в корзину</button>
+                        <img src={FlowerBasketPng} alt=""/>
+                        <button>Add to cart</button>
                     </div>
                     <div class="price">
-                        <span> 145000<span class="p">p</span></span>
-                        <span class="og-price">15400<span class="p">p</span></span>
+                        <span> 100<span class="p">$</span></span>
+                        <span class="og-price">150<span class="p">$</span></span>
                         <span class="sale">-7%</span>
                     </div>
-                    <div class="title">Корзина с цветами</div>
+                    <div class="title">Flower basket</div>
                 </div>
                 <div class="container-item">
                     <div class="thumbanail">
-                        <img src="./images/castle_1.png" alt=""/>
-                        <button>Добавить в корзину</button>
+                        <img src={AquariumPng} alt=""/>
+                        <button>Add to cart</button>
                     </div>
                     <div class="price">
-                        <span> 145000<span class="p">p</span></span>
-                        <span class="og-price">15400<span class="p">p</span></span>
+                        <span> 150<span class="p">$</span></span>
+                        <span class="og-price">200<span class="p">$</span></span>
                         <span class="sale">-7%</span>
                     </div>
-                    <div class="title">Замок для аквариума</div>
+                    <div class="title">Aquarium lock</div>
                 </div>
             </div>
         </section>
@@ -123,23 +135,23 @@ root.render(
     <footer>
         <div class="container">
             <div class="left">
-                <div class="kontackt">Контакты</div>
+                <div class="kontackt">Contact</div>
                 <div class="phone">+7 999 999 99 99</div>
                 <div class="social-netrwork">
                     <div class="social-netrwork-item">
-                        <img class="item-inst" src="./images/instagram 3.png" alt=""/>
+                        <img class="item-inst" src={InstagramPng} alt=""/>
                         <p class="item-instag">instagram</p>
                     </div>
                     <div class="social-netrwork-item">
-                        <img  src="./images/whatsapp 1.png" alt=""/>
+                        <img  src={WhatappPng} alt=""/>
                         <p class="item-whatsapp">WhatsApp</p>
                     </div>
                 </div>
             </div>
             <div class="rigth">
-                <div class="adress">Адрес</div>
-                <div class="city">г. Космонавтов <p >проезд Космонавтов, д.11 </p>  </div>
-                <div class="work-regim"> Режим работы  <p class="work-regim2">Круглосуточно</p> </div>
+                <div class="adress">Address</div>
+                <div class="city">Kosmonavtov <p >proezd Kosmonavtov, 11 </p>  </div>
+                <div class="work-regim"> Working Hours:  <p class="work-regim2">24 hours a day</p> </div>
             </div>
         </div>
     </footer>
